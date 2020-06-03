a48dd17613782976a8db8677a592eaefc2fb3578
PoliteCat420 <45389162+PoliteCat420@users.noreply.github.com>
2020-06-03 23:00:58 +0400

    New

---
+++ script.js
@@ -0,0 +1,13 @@
+let man;
+
+// man = "Yasser";
+man = 120;
+console.log (man);
+
+// document.body.style.color = "orange"
+
+let qu = document.getElementById("quote")
+qu.addEventListener('click', () =>{
+qu.style.color = "red";
+}
+	) 
